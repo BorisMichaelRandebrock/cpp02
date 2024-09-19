4f88c86917689bf438faceb0118081f081fb3ff1
Boris Randebrock <brandebr@student.42barcelona.com>
2024-09-19 14:26:33 +0200

    added more testings to main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,14 +6,17 @@
 /*   By: brandebr <brandebr@42barcelona.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 12:02:29 by brandebr          #+#    #+#             */
-/*   Updated: 2024/09/16 17:57:45 by brandebr         ###   ########.fr       */
+/*   Updated: 2024/09/19 14:25:27 by brandebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int main( void ) {
-	
+	std::cout << "Welcome to this very fun main :)." << std::endl;
+	std::cout << "This main will be way more readable if you comment the constructor/destructor couts." << std::endl;
+	std::cout << std::endl;
+	std::cout << "Subject tests:" << std::endl;
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	std::cout << a << std::endl;
@@ -23,6 +26,37 @@ int main( void ) {
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
-	
+	std::cout << std::endl;
+	std::cout << "a, b: " << a << ", " << b << std::endl;
+	std::cout << std::endl;
+	std::cout << "Comparison tests:" << std::endl;
+	std::cout << "a > b: " << (a > b) << std::endl;
+	std::cout << "a < b: " << (a < b) << std::endl;
+	std::cout << "a >= b: " << (a >= b) << std::endl;
+	std::cout << "a <= b: " << (a <= b) << std::endl;
+	std::cout << "a == b: " << (a == b) << std::endl;
+	std::cout << "a != b: " << (a != b) << std::endl;
+	std::cout << std::endl;
+	std::cout << "Arithmetic tests:" << std::endl ;
+	std::cout << "a + b: " << (a + b) << std::endl;
+	std::cout << "a - b: " << (a - b) << std::endl;
+	std::cout << "a * b: " << (a * b) << std::endl;
+	std::cout << "100 / b: " << (Fixed(100) / b) << std::endl;
+	std::cout << std::endl;
+	std::cout << "min/max tests:" << std::endl;
+	std::cout << "max(a, b): " << Fixed::max(a, b) << std::endl;
+	std::cout << "min(a, b): " << Fixed::min(a, b) << std::endl;
+	std::cout << "max(a, 4): " << Fixed::max(a, Fixed(4)) << std::endl;
+	std::cout << "min(a, 4): " << Fixed::min(a, Fixed(4)) << std::endl;
+/*	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	*/
 	return 0;
 }
